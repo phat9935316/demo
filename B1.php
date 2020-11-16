18d707ff98c6604c8f3327e6998933c9a248a269
Tien Phat <phat9935316@gmail.com>
2020-11-16 17:24:56 +0700

    demo php

--- B1.php
+++ B1.php
@@ -8,12 +8,22 @@
 <body>
     <?php
         if(isset($_POST['submit'])){
-            
+            $text=$_POST['arr'];
+            $arr=explode(",",$text);
+            $total=0;
+            $arr1=array();
+            $n=$_POST['n'];
+            for($i=0;$i<$_POST['n'];$i++){
+                $arr1[]=$arr[$i];
+                $arr1[]=$arr[$n+$i];
+                $arr1[]=$arr[$n*2+$i];
+            }
+            print_r($arr1);
         }
     ?>
     <form action="" method="post">
         <span>Nhap n:</span>
-        <input type="text" name="arr" id="">
+        <input type="text" name="n" id="">
         <span>Nhap mang vd:1,2,3,4,5,5</span>
         <input type="text" name="arr" id="">
         <button name="submit">Submit</button>
